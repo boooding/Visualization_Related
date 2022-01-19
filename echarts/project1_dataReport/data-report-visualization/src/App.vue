4fd3f6e0b861e33023a1a2beb68cb7c7f9281427
booding <booding.xiu@outlook.com>
2022-01-19 20:12:22 +0800

    add: initialize project

---
+++ echarts/project1_dataReport/data-report-visualization/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app, body, html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
