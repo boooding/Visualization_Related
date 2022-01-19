f7b834d57e4273bee3db8de46a56b5b4a740f179
booding <booding.xiu@outlook.com>
2022-01-19 20:43:21 +0800

    add: project initialize with components

--- echarts/project1_dataReport/data-report-visualization/src/App.vue
+++ echarts/project1_dataReport/data-report-visualization/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app, body, html {
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+html, body, #app {
   width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
 }
-
 </style>
--- echarts/project1_dataReport/data-report-visualization/src/views/Home.vue
+++ echarts/project1_dataReport/data-report-visualization/src/views/Home.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <top-view/>
+    <sales-view/>
+    <bottom-view/>
+    <map-view/>
   </div>
 </template>
 
 <script>
+import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
+import BottomView from '../components/BottomView'
+import MapView from '../components/MapView'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    TopView,
+    SalesView,
+    BottomView,
+    MapView
+  }
 }
 </script>
+
+<style>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
